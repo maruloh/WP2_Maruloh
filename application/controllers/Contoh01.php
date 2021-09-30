571ef1d1b7d98b95d5010601b24604f87d594f7f
maruloh <maruloh.mru@gmail.com>
2021-09-30 10:04:34 +0700

    menambahkan file controller Contoh01.php

---
+++ application/controllers/Contoh01.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Contoh01 extends CI_Controller
+{
+
+    public function index()
+    {
+        echo "<h3>Hallo, nama saya Maruloh</h3>";
+        echo "Selamat Belajar Web Programming II";
+    }
+}
